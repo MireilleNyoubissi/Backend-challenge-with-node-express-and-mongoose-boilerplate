ec2a4b0baaee853b3c5a6e2d043ef05870210d57
MireilleNyoubissi <nmireillemat@yahoo.fr>
2023-01-29 01:38:53 +0000

    added get and post request req.params, req.query and req.body plus middleware body-parser

--- myApp.js
+++ myApp.js
@@ -1,25 +1,89 @@
 let express = require("express");
 let app = express();
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const uri = process.env.MONGO_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+//parse application/x-www-form-urlencoded
+app.use((req, res) => {
+    bodyParser.urlencoded({ extended: false });
+});
+
+// parse application/json
+app.use(bodyParser.json());
+
+/*Serve an HTML file*/
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+/*Serve static assets*/
+app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
+
+/*serve a json on a specific route*/
+// app.get("/json", (req, res) => {
+//     res.json(
+//         {"message": "Hello json"}
+//     );
+// });
 
 
+/*use the .env file to configure the app*/
+app.get("/json", (req, res)=> {
+    if(process.env.MESSAGE_STYLE === "uppercase") {
+        res.json(
+            { "message": "HELLO JSON" }
+        )
+    } else {
+        res.json({ "message": "Hello json" });
+    }
+})
 
+/*Mount the logger middleware*/
+app.use((req, res, next) => {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+});
+/*Chaining middleware. A time server*/ 
 
+const middleware = (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+};
 
+app.get("/now",middleware, (req, res) => {
+    res.send({ time: req.time })
+});
 
+/*Get input from client. Route parameter*/
 
+app.get("/:word/echo", (req, res) => {
+    const { word } = req.params;
+    res.json(
+        { echo: word }
+    )
+})
 
+/* Get input from client. Query parameter*/
 
+app.get("/name", (req, res)=> {
+    const { first: firstName, last: lastName } = req.query;
+    res.json({
+       name: `${firstName} ${lastName}` 
+    })
+})
 
+/*Get data from post */
 
-
-
-
-
-
-
-
-
-
+app.post("/name", (req, res) => {
+    const { first: firstName, last: lastName } = req.body;
+    res.json({
+      name: `${firstName} ${lastName}`,
+    });
+}); 
 
 
 
